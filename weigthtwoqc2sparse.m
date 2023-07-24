034553d4d9915d0dbcba449d20ddc7d4068bbf64
Usatyuk Vasiliy <L@lcrypto.com>
2023-07-24 15:01:20 +0300

    Update weigthtwoqc2sparse.m

--- weigthtwoqc2sparse.m
+++ weigthtwoqc2sparse.m
@@ -13,16 +13,17 @@ H = sparse([]);
 for i = 1:m
     lH = sparse([]);
     for j = 1:n
-        shift2=0;
+        shift2=-1;
         shift = fscanf(fid, '%d', [1 1]);
         shift2 = fscanf(fid, '&%d', [1 1]);
         if shift == -1
             lH = [lH Z];
-        else    
-            if shift2~= 0
-                lH = [lH circshift(I, [0 shift])+circshift(I, [0 shift2])];
+        else
+            if shift2~=-1
+                lH = [lH circshift(I, [0 shift])+circshift(I, [0 shift2]) ];
             else
                 lH = [lH circshift(I, [0 shift])];
+             
             end
         end
     end
